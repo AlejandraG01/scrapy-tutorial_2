ad6d8b2731af899d45155cc0dddcdd081a9e40dc
AlejandraG01 <alejandragonzalez0107@gmail.com>
2017-09-11 16:56:02 -0300

    modificando spider-product-buscape

--- tutorial_2/spiders/product-Buscape_spider.py
+++ tutorial_2/spiders/product-Buscape_spider.py
@@ -6,16 +6,27 @@ class ProductBuscapeSpider(scrapy.Spider):
 
    
    def parse(self, response):
+       self.logger.info('Hi, this is my item pages! %s', response.url)
+       l =  ItemLoader(item=Product(), response=response)
+       l.add_css('title','div.details div.description a.link div.is-truncated p.name-prd::text')
+       l.add_css('author','div.details span.carac.carac-autor a.info::text')
+       l.add_css('price','div.price-range a::attr(title)')
+       yield l.load_Item()
        
-           
-       for product in response.css('div.bp-wrap section.proc-search-results'):
+       
+   """    for name in response.css('.details'):
            yield {
-                     'title': product.css('.details div.description a.link div.is-truncated p.name-prd::text').extract_first(),
-                     'author': product.css('.details span.carac.carac-autor a.info::text').extract_first(),
-                     'price': product.css('div.price-range a::attr(title)').extract_first(),
-                     #'image': product.css('.image img::attr(src)').extract(),
+                     'title': name.css('div.description a.link div.is-truncated p.name-prd::text').extract_first(),
+                     'author': name.css('span.carac.carac-autor a.info::text').extract_first(),
+                     
+                     }
+           
+       for price in response.css('.price-range'):
+            yield {
+                     'price': product.css('a::attr(title)').extract_first(),
+                     #'image': product.css('.image img::attr(src)').extract(), 
             }
-
+"""
 
 
         
